5f744322999bc7ffc0d528dc36d2839b8249687e
lambertco <blambertco@gmail.com>
2019-10-08 16:23:12 -0600

    database connected and showing id number

--- customer.js
+++ customer.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var colors = require('colors');
-var Table = require('cli-table');
+
 
 // Connection script
 var connection = mysql.createConnection({
@@ -10,4 +10,9 @@ var connection = mysql.createConnection({
     user: "root",
     password: "root",
     database: "bamazon_DB"
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log(colors.cyan(`You are now connected to the Bamazon Store database as id: ${connection.threadId}`))
 });
